b660a283eaef53d15baef66b578cf88b8e221bf6
Wan Mekwi <w.mekwi@gmail.com>
2026-06-28 01:12:07 +0100

    hymnbeam 0.1.4

--- Casks/hymnbeam.rb
+++ Casks/hymnbeam.rb
@@ -1,6 +1,6 @@
 cask "hymnbeam" do
-  version "0.1.3"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.1.4"
+  sha256 "3315a1156bd07194836ef58b8adb5e1cf448a657069e007b8524f6b43c351c74"
 
   url "https://github.com/wanmekwi/hymnbeam/releases/download/v#{version}/HymnBeam_#{version}_universal.dmg"
   name "HymnBeam"
